e3bbf28f9918cc218594ae5bf8ce60656e3471da
Amit <amitmakhija989@gmail.com>
2021-03-04 11:22:29 +0530

    Add ShowProduct Controller and View

---
+++ MyAamdhani/Controllers/ShowProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAamdhani.Controllers
+{
+    public class ShowProductController : Controller
+    {
+        // GET: ShowProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
