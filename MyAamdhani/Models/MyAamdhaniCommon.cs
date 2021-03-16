b140c553bb7eb62d3394728422ce90698212947f
Amit Makhija <amitmakhija989@gmail.com>
2021-03-16 15:56:23 +0530

    Add Product Manage Page for Add the Product and Show the Product List for Admin with Misc. changes etc.

---
+++ MyAamdhani/Models/MyAamdhaniCommon.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+namespace MyAamdhani.Models
+{
+    public class MyAamdhaniCommon
+    {
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable(typeof(T).Name);
+                //Get all the properties by using reflection 
+                PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                foreach (PropertyInfo prop in Props)
+                    //Setting column names as Property names 
+                    dataTable.Columns.Add(prop.Name);
+
+                foreach (T item in items)
+                {
+                    var values = new object[Props.Length];
+                    for (int i = 0; i < Props.Length; i++)
+                    {
+                        if (Props[i].Name.ToLower() == "isactive")
+                            values[i] = Convert.ToBoolean(Props[i].GetValue(item, null)) == true ? 1 : 0;
+
+                        else if (Props[i].Name.ToLower() == "isdeleted")
+                        {
+                            values[i] = Convert.ToBoolean(Props[i].GetValue(item, null)) == true ? 1 : 0;
+                        }
+                        else if (Props[i].Name.ToLower() == "isverified")
+                            values[i] = Convert.ToBoolean(Props[i].GetValue(item, null)) == true ? 1 : 0;
+
+                        else if (Props[i].Name.ToLower() == "rcdinsts")
+                        {
+                            values[i] = Props[i].GetValue(item, null) != null ? Convert.ToDateTime(Props[i].GetValue(item, null)).ToString("yyyy-MM-dd hh:mm:ss") : DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss");
+                        }
+                        else if (Props[i].Name.ToLower() == "rcdupdtts")
+                        {
+                            values[i] = Props[i].GetValue(item, null) != null ? Convert.ToDateTime(Props[i].GetValue(item, null)).ToString("yyyy-MM-dd hh:mm:ss") : DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss");
+                        }
+                        else
+                            values[i] = Props[i].GetValue(item, null);
+                    }
+                    dataTable.Rows.Add(values);
+                }
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static string GetBase64StringForImage(string imgPath)
+        {
+            byte[] imageBytes = System.IO.File.ReadAllBytes(imgPath);
+            string base64String = Convert.ToBase64String(imageBytes);
+            return base64String;
+        }
+        //public string GetConnectionString()
+        //{
+        //    return appConfig.ConnectionString;
+        //}
+        public static class CustomClaimTypes
+        {
+            public const string MasterFullName = "http://schemas.xmlsoap.org/ws/2014/03/mystuff/claims/masterfullname";
+            public const string MasterUserId = "http://schemas.xmlsoap.org/ws/2014/03/mystuff/claims/masteruserid";
+        }
+
+
+
+        
+        /// <summary>
+        /// Custom Paging for Footable grids
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="pageNoUp"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetPaging(DataTable dt, int pageNoUp, string name = "", string filter = "")
+        {
+
+            StringBuilder htmlPaging = new StringBuilder();
+            
+            int pageSize = 10;// PerformanceAppraisalCommon.GetPageSize();
+
+            int totalcount = Convert.ToInt32(dt.Rows[0]["Rcount"].ToString());
+            var pageNo = 1;
+            htmlPaging.Append("<div class=\"pagination-footer\">");
+
+            htmlPaging.Append("<div class=\"row\">");
+            htmlPaging.Append("<div class=\"col-md-4\">");
+            htmlPaging.Append("<span style=\"margin-left: 15px;\">Showing</span> " + Convert.ToInt32(dt.Rows[0]["rownum"].ToString()) + " <span>to</span> " + Convert.ToInt32(dt.Rows[dt.Rows.Count - 1]["rownum"].ToString()) + "<span> of </span> " + totalcount + " <span> entries </span>");
+            htmlPaging.Append("</div>");
+            htmlPaging.Append("<div class=\"col-md-8\" style=\"text-align: right; \">");
+            htmlPaging.Append("<div class=\"pagination-container\">");
+            htmlPaging.Append("<nav aria-label=\"Page navigation example\">");
+            totalcount = totalcount / pageSize + (totalcount % pageSize != 0 ? 1 : 0);
+
+            //pageNo = Convert.ToInt32(dt.Rows[0]["rcount"].ToString());
+            htmlPaging.Append("<ul class=\"pagination pagination-sm\">");
+            int skip = 0;
+            if (pageNoUp != 1)
+                skip = Convert.ToInt32(pageNoUp + "0");
+
+            //link = link + "<ul class='pagination center'><li> <a onclick=pagination('" + ds.Tables[1].Rows[0]["PageisShow"] + "','" + Ds.Tables[1].Rows[0]["PageSize"] + "','" + 1 + "')>First</a></li>";
+            if (pageNoUp > 1)
+            {
+
+                //link = link + "<li> <a onclick=pagination('" + ds.Tables[1].Rows[0]["PageisShow"] + "','" + ds.Tables[1].Rows[0]["PageSize"] + "','" + (pageNo > 1 ? pageNo - 1 : 1) + "')>Previous</a></li>";
+                htmlPaging.Append(
+               "<li class=\"page-item\">");
+                if (string.IsNullOrEmpty(name))
+                    htmlPaging.Append("<a data-powertip=\"First\" class=\"page-link tooltips\"  href=\"javascript:void(0);\" onclick=\"SearchFilter('1','" + filter + "')\"  rel=\"prev\"><i class=\"fa fa-angle-double-left\" style=\"padding-top:5px;padding-left:0;\"></i></a></li>");
+                else
+                    htmlPaging.Append("<a data-powertip=\"First\" class=\"page-link tooltips\"  href=\"javascript:void(0);\" onclick=\"SearchFilter('1','" + filter + "')\"  rel=\"prev\"><i class=\"fa fa-angle-double-left\" style=\"padding-top:5px;padding-left:0;\"></i></a></li>");
+                htmlPaging.Append("<li class=\"page-item\">");
+                htmlPaging.Append("<input type=\"hidden\" value=" + (pageNoUp > 1 ? pageNoUp - 1 : 1) + " />");
+                if (string.IsNullOrEmpty(name))
+                    htmlPaging.Append("<a data-powertip=\"Previous\" class=\"page-link tooltips\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + Convert.ToInt32((pageNoUp > 1 ? pageNoUp - 1 : 1)) + "','" + filter + "')\" rel=\"prev\"><i class=\"fa fa-angle-left\" style=\"padding-top:5px;padding-left:0;\"></i></a>");
+                else
+                    htmlPaging.Append("<a data-powertip=\"Previous\" class=\"page-link tooltips\" title=\"Previous\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + Convert.ToInt32((pageNoUp > 1 ? pageNoUp - 1 : 1)) + "','" + filter + "')\" rel=\"prev\"><i class=\"fa fa-angle-left\" style=\"padding-top:5px;padding-left:0;\"></i></a>");
+                htmlPaging.Append("</li>");
+            }
+            var PageStart = 0;
+            if (totalcount <= pageNoUp + 4)
+            {
+                var sub = (pageNoUp + 4) - totalcount;
+                PageStart = pageNoUp - sub;
+            }
+            else
+                PageStart = pageNoUp;
+            for (int i = PageStart > 5 ? PageStart : 1; i <= (PageStart <= 5 ? 5 : PageStart + 4); i++)
+            //for (int i = pageNo; i <= (pageNo + 4); i++)
+            {
+                //if (totalcount <= pageNo + 4)
+                //{
+                //    var sub = (pageNo + 4) - totalcount;
+                //    i = pageNo - sub;
+                //}
+                if (i <= totalcount)
+                {
+                    if (i == pageNoUp)
+                    {
+                        htmlPaging.Append("<li class=\"page-item active\">");
+                        if (string.IsNullOrEmpty(name))
+                            htmlPaging.Append("<a href=\"javascript:void(0);\" class=\"page-link\" onclick=\"SearchFilter('" + i + "','" + filter + "')\">" + i + "</a>");
+                        else
+                            htmlPaging.Append("<a href=\"javascript:void(0);\" class=\"page-link\" onclick=\"SearchFilter('" + i + "','" + filter + "')\">" + i + "</a>");
+                        htmlPaging.Append("</li>");
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(name))
+                            htmlPaging.Append("<li class=\"page-item\"><a class=\"page-link\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + i + "','" + filter + "')\">" + i + "</a></li>");
+                        else
+                            htmlPaging.Append("<li class=\"page-item\"><a class=\"page-link\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + i + "','" + filter + "')\">" + i + "</a></li>");
+                    }
+
+                }
+                else
+                    break;
+            }
+            
+            if (pageNoUp < totalcount)
+            {
+                //link = link + "<li> <a onclick=pagination('" + Ds.Tables[1].Rows[0]["PageisShow"] + "','" + Ds.Tables[1].Rows[0]["PageSize"] + "','" + (PageIndex < totalcount ? PageIndex + 1 : totalcount) + "')>Next</a></li>";
+                htmlPaging.Append("<li class=\"page-item\">");
+
+                if (string.IsNullOrEmpty(name))
+                    htmlPaging.Append("<a data-powertip=\"Next\" class=\"page-link tooltips\"  href=\"javascript:void(0);\" onclick=\"SearchFilter('" + (Convert.ToInt32(pageNoUp < totalcount ? pageNoUp + 1 : totalcount)) + "','" + filter + "','" + (skip) + "')\"  rel=\"next\"><i class=\"fa fa-angle-right\" style=\"padding-top:5px;padding-left:2px;\"></i></a>");
+                else
+                    htmlPaging.Append("<a data-powertip=\"Next\" class=\"page-link tooltips\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + (Convert.ToInt32(pageNoUp < totalcount ? pageNoUp + 1 : totalcount)) + "','" + filter + "','" + (skip) + "')\"  rel=\"next\"><i class=\"fa fa-angle-right\" style=\"padding-top:5px;padding-left:2px;\"></i></a>");
+                htmlPaging.Append("</li>");
+                htmlPaging.Append("<li class=\"page-item\">");
+                if (string.IsNullOrEmpty(name))
+                    htmlPaging.Append("<a  data-powertip=\"Last\" class=\"page-link tooltips\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + totalcount + "','" + filter + "','" + (skip) + "')\"  rel=\"next\"><i class=\"fa fa-angle-double-right\" style=\"padding-top:5px;padding-left:2px;\"></i></a>");
+                else
+                    htmlPaging.Append("<a  data-powertip=\"Last\" class=\"page-link tooltips\" href=\"javascript:void(0);\" onclick=\"SearchFilter('" + totalcount + "','" + filter + "','" + (skip) + "')\"  rel=\"next\"><i class=\"fa fa-angle-double-right\" style=\"padding-top:5px;padding-left:2px;\"></i></a>");
+
+                htmlPaging.Append("</li>");
+            }
+            //link = link + "<li>  <a onclick=pagination('" + Ds.Tables[1].Rows[0]["PageisShow"] + "','" + Ds.Tables[1].Rows[0]["PageSize"] + "','" + totalcount + "')>Last</a></li> </ul>";
+
+            htmlPaging.Append("</nav></ul>");
+            htmlPaging.Append("</div>");
+            htmlPaging.Append("</div>");
+            htmlPaging.Append("</div>");
+
+ 
+            return htmlPaging.ToString();
+        }
+    }
+}
---
+++ MyAamdhani/Models/ProductEntityModel.cs
@@ -0,0 +1,162 @@
+﻿using MyAamdhani.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAamdhani.Models
+{
+
+    public class SelectAllProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public int rownum { get; set; }
+        public Guid UniqueId { get; set; }
+        public int RCount { get; set; }
+    }
+    public class ProductEntityModel
+    {
+        MyAamdhaniEntities db = new MyAamdhaniEntities();
+
+        public object GetFabric()
+        {
+            try
+            {
+                var Fabric = db.tbl_Fabric.Select(s => new { FabricId = s.Fabric_Id, FabricName = s.Fabric_Name }).ToList();
+                return Fabric;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+        public object GetPattern()
+        {
+            try
+            {
+                var Pattern = db.tbl_Pattern.Select(s => new { PatternId = s.Pattern_Id, PatternName = s.Pattern_Name}).ToList();
+                return Pattern;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+        public object GetSareeBorder()
+        {
+            try
+            {
+                var SareeBorder = db.tbl_SareeBorder.Select(s => new { SareeBorderId = s.SareeBorder_Id, SareeBorderName = s.SareeBorder_Name }).ToList();
+                return SareeBorder;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+        public object GetStyle()
+        {
+            try
+            {
+                var Style = db.tbl_ClothStyle.Select(s => new { StyleId = s.Style_Id, StyleName = s.Style_Name }).ToList();
+                return Style;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+        public List<SelectListItem> GetLength()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem
+            {
+                Text = "5.25 m",
+                Value = "1"
+            });
+            items.Add(new SelectListItem
+            {
+                Text = "5.5 m",
+                Value = "2"
+            });
+            items.Add(new SelectListItem
+            {
+                Text = "6.0 m",
+                Value = "3"
+            });
+            return items;
+        }
+
+        public List<SelectAllProduct> GetProduct(int skip, int take, string whereQuery, string SortQuery)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                using (SqlConnection connection = new SqlConnection(db.Database.Connection.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand("Sp_GetAllProduct", connection))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                        adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                        adapter.SelectCommand.CommandTimeout = 0;
+                        adapter.SelectCommand.Parameters.Add(new SqlParameter("@action", "GetAllProducts"));
+                        adapter.SelectCommand.Parameters.Add(new SqlParameter("@WhereQuery", whereQuery));
+                        adapter.SelectCommand.Parameters.Add(new SqlParameter("@SortQuery", SortQuery));
+                        adapter.SelectCommand.Parameters.Add(new SqlParameter("@RowFrom", skip));
+                        adapter.SelectCommand.Parameters.Add(new SqlParameter("@RowTo", take));
+                        adapter.Fill(dt);
+
+                    }
+                    connection.Close();
+                }
+                //SqlParameter[] param = new SqlParameter[5];
+                //param[0] = new SqlParameter("@ProductId", 0);
+                //param[1] = new SqlParameter("@WhereQuery", whereQuery);
+                //param[2] = new SqlParameter("@SortQuery", SortQuery);
+                //param[3] = new SqlParameter("@RowFrom", skip + 1);
+                //param[4] = new SqlParameter("@RowTo", skip + take);
+                //dt = DBHelper.BindDataTableWithParams(DBHelper.LoadStoredProcedure(db, "Sp_GetAllProducts"), param);
+                List<SelectAllProduct> productList = new List<SelectAllProduct>();
+                productList = new List<SelectAllProduct>(from DataRow dr in dt.Rows
+                                                         select new SelectAllProduct()
+                                                         {
+                                                             ProductId = Convert.ToInt32(dr["ProductId"]),
+                                                             //DateAdded = Convert.ToDateTime(dr["NoticeDate"]).ToString("yyyy-MM-dd"),//Need to Convert.ToString() in App
+                                                             Image = Convert.ToString(dr["Image"]),
+                                                             Quantity = Convert.ToInt32(dr["Quantity"]),
+                                                             ProductName = Convert.ToString(dr["ProductName"]),
+                                                             Price = Convert.ToInt32(dr["Price"]),
+                                                             IsActive = Convert.ToBoolean(dr["IsActive"]),
+                                                             RCount = Convert.ToInt32(dr["RCount"]),
+                                                             rownum = Convert.ToInt32(dr["rownum"]),
+                                                             UniqueId = !string.IsNullOrEmpty(Convert.ToString(dr["UniqueId"])) ? Guid.Parse(Convert.ToString(dr["UniqueId"])) : Guid.Parse("00000000-0000-0000-0000-000000000000"),
+                                                         }).ToList();
+                return productList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
